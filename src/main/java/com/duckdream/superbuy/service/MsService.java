55db42175ff61871157d15728f231b6f2eb300c1
zhumengstar <1378484330@qq.com>
2019-02-27 10:25:53 +0800

    update

--- src/main/java/com/duckdream/superbuy/service/MsService.java
+++ src/main/java/com/duckdream/superbuy/service/MsService.java
@@ -5,12 +5,19 @@ import com.duckdream.superbuy.entity.OrderInfo;
 import com.duckdream.superbuy.entity.User;
 import com.duckdream.superbuy.redis.MsKey;
 import com.duckdream.superbuy.redis.RedisService;
+import com.duckdream.superbuy.util.MD5Util;
+import com.duckdream.superbuy.util.UUIDUtil;
 import com.duckdream.superbuy.vo.GoodsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class MsService {
@@ -61,5 +68,96 @@ public class MsService {
     public void reset(List<GoodsVO> goodsList) {
         goodsService.resetStock(goodsList);
         orderService.deleteOrders();
+    }
+
+
+    public boolean checkPath(User user, long goodsId, String path) {
+        if(user == null || path == null) {
+            return false;
+        }
+        String pathOld = redisService.get(MsKey.getMsPath, ""+user.getId()+"_"+goodsId, String.class);
+        return path.equals(pathOld);
+    }
+
+    public String createMsPath(User user, long goodsId) {
+        if(user == null || goodsId <= 0) {
+            return null;
+        }
+        String str = MD5Util.md5(UUIDUtil.uuid() + "123456");
+        redisService.set(MsKey.getMsPath, ""+user.getId()+"_"+goodsId, str);
+        return str;
+    }
+
+    public BufferedImage createMsVerifyCode(User user, long goodsId) {
+        if(user == null || goodsId <= 0) {
+            return null;
+        }
+        int width = 80;
+        int height = 32;
+        //create the image
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        // set the background color
+        g.setColor(new Color(0xDCDCDC));
+        g.fillRect(0, 0, width, height);
+        // draw the border
+        g.setColor(Color.black);
+        g.drawRect(0, 0, width - 1, height - 1);
+        // create a random instance to generate the codes
+        Random rdm = new Random();
+        // make some confusion
+        for (int i = 0; i < 50; i++) {
+            int x = rdm.nextInt(width);
+            int y = rdm.nextInt(height);
+            g.drawOval(x, y, 0, 0);
+        }
+        // generate a random code
+        String verifyCode = generateVerifyCode(rdm);
+        g.setColor(new Color(0, 100, 0));
+        g.setFont(new Font("Candara", Font.BOLD, 24));
+        g.drawString(verifyCode, 8, 24);
+        g.dispose();
+        //把验证码存到redis中
+        int rnd = calc(verifyCode);
+        redisService.set(MsKey.getMsVerifyCode, user.getId()+","+goodsId, rnd);
+        //输出图片
+        return image;
+    }
+
+    public boolean checkVerifyCode(User user, long goodsId, int verifyCode) {
+        if(user == null || goodsId <= 0) {
+            return false;
+        }
+        Integer codeOld = redisService.get(MsKey.getMsVerifyCode, user.getId()+","+goodsId, Integer.class);
+        if(codeOld == null || codeOld - verifyCode != 0 ) {
+            return false;
+        }
+        redisService.delete(MsKey.getMsVerifyCode, user.getId()+","+goodsId);
+        return true;
+    }
+
+    private static int calc(String exp) {
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+            return (Integer)engine.eval(exp);
+        }catch(Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    private static char[] ops = new char[] {'+', '-', '*'};
+    /**
+     * + - *
+     * */
+    private String generateVerifyCode(Random rdm) {
+        int num1 = rdm.nextInt(10);
+        int num2 = rdm.nextInt(10);
+        int num3 = rdm.nextInt(10);
+        char op1 = ops[rdm.nextInt(3)];
+        char op2 = ops[rdm.nextInt(3)];
+        String exp = ""+ num1 + op1 + num2 + op2 + num3;
+        return exp;
     }
 }
